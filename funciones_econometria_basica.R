98032130c6b06a441280efb6a390302f9881f78f
Ignacio <fernando.marqueznail@gmail.com>
2022-10-19 13:54:59 -0300

    commit 3

--- funciones_econometria_basica.R
+++ funciones_econometria_basica.R
@@ -6,17 +6,17 @@ theme_set(theme_classic() + theme(plot.title = element_text(hjust = .5)))
 
 exploratorio <- function(data, x, y) {
   require(ggplot2)
-  valor_x <- data |> select({{ x }}); valor_x <- valor_x[,1];
+  valor_x <- data |> select({{ x }}) ;valor_x <- valor_x[,1];
   valor_y <- data |> select({{ y }}); valor_y <- valor_y[,1];
   
   
-  grafico_linea <- grafico_puntos(data, {{ x }}, {{ y }})
+  grafico_puntoz <- grafico_puntos(data, {{ x }}, {{ y }})
   histograma_x <- grafico_histograma(data, {{ x }})  
   histograma_y <- grafico_histograma(data, {{ y }})  
   tabla_resumen_x <- estadistica_univariante(valor_x)
   tabla_resumen_y <- estadistica_univariante(valor_y)
   
-  lista <- list("grafico_puntos" = grafico_linea, "histograma_x" = histograma_x, 
+  lista <- list("grafico_puntos" = grafico_puntos, "histograma_x" = histograma_x, 
                 "histograma_y" = histograma_y, "tabla_resumen_x" = tabla_resumen_x,
                 "tabla_resumen_y" = tabla_resumen_y)
   return(lista)
@@ -25,7 +25,8 @@ exploratorio <- function(data, x, y) {
 grafico_histograma <- function(data, variable) {
   x <- dplyr::select(data, {{ variable }})
   ancho <- calcular_ancho(x[,1])
-  return (ggplot(data, aes(x = {{ variable }} )) + geom_histogram(binwidth = ancho, color = "black", fill = "white") + 
+  return (ggplot(data, aes(x = {{ variable }} )) + 
+            geom_histogram(binwidth = ancho, color = "black", fill = "white") + 
             geom_density(aes(y = ..count..*ancho), color = "red"))
 }
 
@@ -45,7 +46,6 @@ estadistica_univariante <- function(x) {
 }
 
 
-
 # COMPLETAR PARA LA FUNCION EXPLORATORIO ----------
 
 estadistica_bivariante <- function(x, y) {
@@ -59,11 +59,16 @@ estadistica_bivariante <- function(x, y) {
 modelar <- function(datos, x, y) {
   
   modelo <- lm(paste(y, '~', x), data = datos)
-  hisograma_residuos <- grafico_residuos(modelo) 
+  histograma_residuos <- grafico_residuos(modelo) 
+  grafico_residuos_ajustados <- grafico_homocedasticidad(modelo)
+  
   h_residuos <- hipotesis_residuos_normal(residuals(modelo))
   h_homocedasticidad <- hipotesis_homocedasticidad(modelo)
-  lista <- list("modelo" = modelo, residuos = hisograma_residuos,"h_residuos" = h_residuos,
-                "h_homocedasticidad" = h_homocedasticidad)
+  h_autocorrelacion <- hipotesis_autocorrelacion(modelo)
+  lista <- list("modelo" = modelo, residuos = histograma_residuos,"h_residuos" = h_residuos,
+                "h_homocedasticidad" = h_homocedasticidad,
+                "g_homocedasticidad" = grafico_residuos_ajustados,
+                "h_autocorrelacion" = h_autocorrelacion)
   return(lista)  
 }
 
@@ -73,7 +78,7 @@ grafico_residuos <- function(modelo) {
   p <- ggplot(modelo, aes(x = residuals(modelo))) + 
     geom_histogram(color = "black", fill = "white", binwidth = ancho) +
     geom_density(aes(y = ..count..* ancho ), color = "red", size = 1.2) + 
-    geom_vline(xintercept = mean(residuals(modelo)), color = "red")
+    geom_vline(xintercept = mean(residuals(modelo)), color = "blue")
   return(p)
 }
 
@@ -94,16 +99,18 @@ hipotesis_homocedasticidad <- function(modelo) {
 
 # COMPLETAR PARA FUNCION MODELAR ---------------------------------------------
 
-grafico_homocedasticidad <- function() {
-  # escribe tu codigo
+grafico_homocedasticidad <- function(modelo) {
   
-  return()
+  p <- ggplot(modelo, aes(x = fitted(modelo), y = residuals(modelo))) + geom_point() +
+    labs(x = "valores ajustados", y = "residuos", title = "Residuos VS valores predichos")
+  
+  return(p)
 }
 
-hipotesis_autocorrelacion <- function() {
+hipotesis_autocorrelacion <- function(modelo) {
   # escribe tu codigo
   
-  return()
+  return(lmtest::dwtest(modelo))
 }
 
 grafico_autocorrelacion <- function() {
